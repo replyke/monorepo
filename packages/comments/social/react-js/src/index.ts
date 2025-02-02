23a3fb0bdc4fe5ec465c71f7015537879d98183e
Yanay Zabary <yanay.tsabary@gmail.com>
2025-02-02 16:43:58 +0200

    export all react stuff

--- packages/comments/social/react-js/src/index.ts
+++ packages/comments/social/react-js/src/index.ts
@@ -1,3 +1,4 @@
+export * from "@replyke/ui-core-react-js";
 export * from "@replyke/comments-social-core";
 export * from "./components";
 export { default as useSocialComments } from "./hooks/useSocialComments";
