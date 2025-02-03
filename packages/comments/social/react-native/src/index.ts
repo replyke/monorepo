644b117792a846aa86ee0cb468cb1c1c99dacfd0
Yanay Zabary <yanay.tsabary@gmail.com>
2025-02-03 13:30:17 +0200

    versions and exports

--- packages/comments/social/react-native/src/index.ts
+++ packages/comments/social/react-native/src/index.ts
@@ -1,4 +1,3 @@
-export * from "@replyke/ui-core-react-native";
 export * from "@replyke/comments-social-core";
 export * from "./components";
 export { default as useSocialComments } from "./hooks/useSocialComments";
