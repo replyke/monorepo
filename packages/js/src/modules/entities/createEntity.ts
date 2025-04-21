81ccd52b8e184ac02cfc6570180f53665451caab
Yanay Zabary <yanay.tsabary@gmail.com>
2025-04-21 11:13:41 +0300

    first init

---
+++ packages/js/src/modules/entities/createEntity.ts
@@ -0,0 +1,27 @@
+import { ReplykeHttpClient } from "../../core/client";
+
+export interface CreateEntityProps {
+  foreignId?: string;
+  resourceId?: string;
+  title?: string;
+  content?: string;
+  attachments?: any[];
+  keywords?: string[];
+  location?: {
+    latitude: number;
+    longitude: number;
+  };
+  metadata?: Record<string, any>;
+  userId?: string;
+}
+
+
+// TODO: Replace "any" with Entity once we have types here too
+export async function createEntity(
+  client: ReplykeHttpClient,
+  data: CreateEntityProps
+): Promise<any> {
+  const path = `/entities`; // assuming client handles prefix like /{projectId}
+  const response = await client.instance.post<any>(path, data);
+  return response.data;
+}
