7c70b72a04bb7b2edc2c0536409377ca7d1bd9db
Yanay Zabary <yanay.tsabary@gmail.com>
2025-04-21 11:15:48 +0300

    removes

--- packages/js/src/modules/comments/index.ts
+++ packages/js/src/modules/comments/index.ts
@@ -1,13 +1,4 @@
-import { createComment } from "./createComment";
 import { fetchComment } from "./fetchComment";
 import { fetchCommentByForeignId } from "./fetchCommentByForeignId";
-import { updateComment } from "./updateComment";
-import { deleteComment } from "./deleteComment";
 
-export {
-  createComment,
-  fetchComment,
-  fetchCommentByForeignId,
-  updateComment,
-  deleteComment,
-};
+export { fetchComment, fetchCommentByForeignId };
