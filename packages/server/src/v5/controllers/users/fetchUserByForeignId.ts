9ba1fb2d68f78e5d6593f436d6f111c8a0a57dfc
Yanay Zabary <yanay.tsabary@gmail.com>
2025-05-23 22:23:59 +0300

    update fetch user by foreign id on backend

--- packages/server/src/v5/controllers/users/fetchUserByForeignId.ts
+++ packages/server/src/v5/controllers/users/fetchUserByForeignId.ts
@@ -1,37 +1,10 @@
 import { Request as ExReq, Response as ExRes } from "express";
 import { User } from "../../../models";
 import IUser from "../../../interfaces/IUser";
-import deepEqual from "../../../utils/deepEqual";
-
-function getQueryString(value: unknown): string | undefined {
-  if (typeof value === "string") return value;
-  return undefined;
-}
-
-function getQueryJSON<T>(value: unknown): T | undefined {
-  if (typeof value === "string") {
-    try {
-      return JSON.parse(value);
-    } catch {
-      return undefined;
-    }
-  }
-  return undefined;
-}
 
 export default async (req: ExReq, res: ExRes) => {
   try {
-    const foreignId = getQueryString(req.query.foreignId);
-    const name = getQueryString(req.query.name);
-    const username = getQueryString(req.query.username);
-    const avatar = getQueryString(req.query.avatar);
-    const bio = getQueryString(req.query.bio);
-    // const birthdateRaw = getQueryString(req.query.birthdate);
-    // const locationRaw = getQueryString(req.query.location);
-    const metadata = getQueryJSON<Record<string, any>>(req.query.metadata);
-    const secureMetadata = getQueryJSON<Record<string, any>>(
-      req.query.secureMetadata
-    );
+    const { foreignId } = req.query;
 
     if (!foreignId || typeof foreignId !== "string") {
       res.status(400).json({
@@ -42,49 +15,17 @@ export default async (req: ExReq, res: ExRes) => {
     }
 
     const projectId = req.project.id;
+
     let user = (await User.findOne({
       where: { foreignId, projectId },
     })) as IUser | null;
 
-    let created = false;
-    let updated = false;
-
     if (!user) {
-      // Create a new user
-      user = (await User.create({
-        projectId,
-        foreignId,
-        name,
-        username,
-        avatar,
-        bio,
-        // location,
-        // birthdate,
-        metadata,
-        secureMetadata,
-      })) as IUser;
-      created = true;
-    } else {
-      // Check for field updates
-      const fieldsToUpdate: Partial<IUser> = {};
-      if (name && user.name !== name) fieldsToUpdate.name = name;
-      if (username && user.username !== username)
-        fieldsToUpdate.username = username;
-      if (avatar && user.avatar !== avatar) fieldsToUpdate.avatar = avatar;
-      if (bio && user.bio !== bio) fieldsToUpdate.bio = bio;
-      // if (location && user.location !== location)
-      //   fieldsToUpdate.location = location;
-      // if (birthdate && user.birthdate !== birthdate)
-      //   fieldsToUpdate.birthdate = birthdate;
-      if (!deepEqual(user.metadata, metadata ?? {}))
-        fieldsToUpdate.metadata = metadata || {};
-      if (!deepEqual(user.secureMetadata, secureMetadata ?? {}))
-        fieldsToUpdate.secureMetadata = secureMetadata || {};
-
-      if (Object.keys(fieldsToUpdate).length > 0) {
-        await user.update(fieldsToUpdate);
-        updated = true;
-      }
+      res.status(404).json({
+        error: "User not found",
+        code: "user/not-found",
+      });
+      return;
     }
 
     // Fetch user again without sensitive fields
@@ -102,11 +43,7 @@ export default async (req: ExReq, res: ExRes) => {
       },
     });
 
-    res.status(created ? 201 : 200).json({
-      user: cleanUser?.toJSON(),
-      created,
-      updated,
-    });
+    res.status(200).json(cleanUser?.toJSON());
   } catch (err: any) {
     console.error("Error fetching/creating/updating user:", err);
     res.status(500).json({
